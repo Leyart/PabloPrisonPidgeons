1d4106cf543f212cb8ad7686f33ed63b2988fabc
crypt <francesco.fiduccia@gmail.com>
2018-01-26 22:45:10 +0100

    Pidgeon and GameScene

---
+++ Assets/Game/Scripts/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+	
+	 void Start() {
+		Button button = gameObject.GetComponent<Button>();
+		button.onClick.AddListener (OnClick);
+	}
+
+  	void OnClick() {
+		SceneManager.LoadScene ("game");
+		loadGameLevel (1);
+	}
+	void loadGameLevel(int level ) {
+		//Object level = TextHelper.loadLevel(level);
+		//for level.counts
+		// new Pidgeon();
+		// set
+	}
+}
---
+++ Assets/Game/Scripts/Pidgeon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pidgeon : MonoBehaviour {
+
+	public float x;
+	public float y;
+	public float speed;
+
+	// 2 elements for the rotation and 1 for the scaling.
+	public Vector3 rotation;
+	public string text;
+	public Object path;
+
+	public Pidgeon() {
+
+	}
+
+	void Update() {
+		// path.getUpdatedPosition (x,y, speed);
+	}
+
+}
