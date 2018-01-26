3db6600bc52b13426b3d7845d138233952215fbf
Alberto Congiu <4lbertoc@gmail.com>
2018-01-26 21:41:58 +0100

    scripts folder

---
+++ Assets/Resources/Scripts/KeyboardTyper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardTyper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			string[] characters = Input.inputString.Split();
+			foreach(string c in characters) {
+				Debug.Log("Pressed " + c);
+			}
+		}
+	}
+}
