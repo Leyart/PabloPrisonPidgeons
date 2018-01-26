30a80fc601ce13c7efeecd08b58fe41cd4e9ca73
Clea Benz <benz@ubique.ch>
2018-01-27 00:04:30 +0100

    pigeon fly

--- Assets/Game/Scripts/LoadScene.cs
+++ Assets/Game/Scripts/LoadScene.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour {
 	
 	 void Start() {
-		Button button = gameObject.GetComponent<Button>();
-		button.onClick.AddListener (OnClick);
+	//	Button button = gameObject.GetComponent<Button>();
+	//	button.onClick.AddListener (OnClick);
 	}
 
   	void OnClick() {
