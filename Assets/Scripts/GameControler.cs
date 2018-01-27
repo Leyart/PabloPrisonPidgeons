c0c4eb2b61d9a0a82e382a359f3ccd3921728d7f
crypt <francesco.fiduccia@gmail.com>
2018-01-27 18:01:28 +0100

    Scrolls

--- Assets/Scripts/GameControler.cs
+++ Assets/Scripts/GameControler.cs
@@ -13,11 +13,19 @@ public class GameControler : MonoBehaviour {
 
 	private int maxNumberToFail = 3;
 
+	private Animator scoreAnimator;
+	private Animator transmittedAnimator;
+
 	public void StartGame(){
 		canvas.enabled = false;
 		GetComponent<PigeonSpawner> ().loadGameLevel (1);
 		score = 0;
 		transmittedMessagesCount = 0;
+
+		scoreAnimator = scoreText.GetComponentInChildren<Animator> ();
+		scoreAnimator.SetBool ("isGreen", true);
+		transmittedAnimator = transmittedMessages.GetComponentInChildren<Animator> ();
+		transmittedAnimator.SetBool ("isRed", true);
 		UpdateScoreView ();
 		UpdateTransmissionView ();
 
@@ -35,6 +43,7 @@ public class GameControler : MonoBehaviour {
 	}
 	private void UpdateScoreView(){
 		scoreText.GetComponent<TextMesh>().text = " " +score;
+		scoreAnimator.SetTrigger ("trigger");
 	}
 	public void UpdateScoreCount(){
 		score++;
@@ -44,6 +53,9 @@ public class GameControler : MonoBehaviour {
 
 	private void UpdateTransmissionView(){
 		transmittedMessages.GetComponent<TextMesh>().text = " " + transmittedMessagesCount;
+
+		transmittedAnimator.SetTrigger ("trigger");
+
 	}
 	public void UpdateTransmissionCount(){
 		transmittedMessagesCount++;
