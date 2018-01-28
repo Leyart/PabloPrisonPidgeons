e46c3eb0123fa57ab9093c71ca5ccd311eb6496e
Clea Benz <benz@ubique.ch>
2018-01-28 14:14:43 +0100

    Musci and layout

---
+++ Assets/MainMenuAudio.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAudio : MonoBehaviour
+{
+	private static MainMenuAudio instance;
+
+	void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Start()
+	{
+		//if (!instance.audio.isPlaying)
+		//	audio.Play();
+	}
+
+	public void turnMusicOff()
+	{
+		if (instance != null)
+		{
+			//if (instance.audio.isPlaying)
+			//    instance.audio.Stop();
+			Destroy(this.gameObject);
+			instance = null;
+		}
+	}
+
+	void OnApplicationQuit()
+	{
+		instance = null;
+	}
+}
